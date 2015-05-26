6e7ec3d7c2eb2756c707e4f1db726fc0d45ad012
Andy Lulham <a.lulham@gmail.com>
2015-05-26 10:38:54 +0100

    Remove some hardcoded years

--- lib/google_drive/financial_metrics.rb
+++ lib/google_drive/financial_metrics.rb
@@ -47,10 +47,9 @@ class FinancialMetrics
   def self.bookings(year)
     block = Proc.new { |x| integize(x) }
     if year.nil?
-      metrics_sum([
-        ['Total bookings', 2014],
-        ['Total bookings', 2013]
-      ], block)
+      years.inject(0) do |memo, year|
+        memo += metrics_cell 'Total bookings', year, block, 'actual'
+      end
     else
       metrics_cell('Total bookings', year, block)
     end
@@ -58,11 +57,9 @@ class FinancialMetrics
 
   def self.income(year, month)
     if year.nil? && month.nil?
-      block = Proc.new { |x| integize(x) }
-      metrics_sum([
-        ['Income', 2014],
-        ['Income', 2013]
-      ], block)
+      years.inject(0) do |memo, year|
+        memo += metrics_cell 'Income', year, Proc.new { |x| integize(x) }, 'actual'
+      end
     else
       metric_with_target('Income', year, month, Proc.new {|x| x.to_f})
     end
